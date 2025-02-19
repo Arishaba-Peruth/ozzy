8d00ad1d764d4b30f5139285e74053d3b410680c
Arishaba-Peruth <lorisperutha@gmail.com>
2025-02-19 16:53:10 +0300

    this is the original status

---
+++ example1.py
@@ -0,0 +1,14 @@
+#syntax about variable names in python
+#a name given to a partition through programming is called variable.
+num1 = 65 #num1 is a variable
+num2 = 100 #num2 is a variable
+print(num1)
+print(num2)
+print(num1 + num2)
+#the name of the variable should not start with a numeral or number e.g 1num or 2num
+#a variable name should not start with a special character e.g @num, &num, !num
+#compound variable names should not have spaces e.g num 1, num 2, my num
+#compound variable names should be joined by either an underscore or camel case or camel notation e.g my_num, num_1, myNum(this is called camel notation)
+#avoid using capital letters to begin a variable name e.g Num1, Num2
+#dont use special words as variable names e.g print, input, for etc
+#variables in python are case senstive e.g Num1 is different from num1
